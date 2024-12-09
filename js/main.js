aee7ef116dd5589ce0d6c2a621cd55a3371460a0
ArToe Group <magezashalom0@gmail.com>
2024-12-09 19:24:10 +0000

    Any Updates

--- js/main.js
+++ js/main.js
@@ -74,8 +74,3 @@ faqs.forEach(faq => {
     }
   });
 });
-
-// change navbar style on scroll
-window.addEventListener("scroll", () => {
-  document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
-});
